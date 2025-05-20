cc1ef12aa94ebb6bd21ea79216c12235dbb4d109
Muhammad Azqi Madani Ardan <muhammadazqi098@gmail.com>
2025-05-20 15:26:56 +0700

    fist commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,6 @@
 import Image from 'next/image'
 
 export default function Home() {
-    const drama = 'azqi'
-
     return (
         <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
             <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
@@ -16,7 +14,6 @@ export default function Home() {
                 />
                 <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
                     <li className="mb-2 tracking-[-.01em]">
-                        {drama}
                         <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
                             src/app/page.tsx
                         </code>
