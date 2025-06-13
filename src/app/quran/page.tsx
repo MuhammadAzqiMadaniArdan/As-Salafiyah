2f86b7d47efec1cc7a3f4b7ca2aa1f2f73eae3b3
Muhammad Azqi Madani Ardan <muhammadazqi098@gmail.com>
2025-06-13 17:26:19 +0700

    update slicing & consume

--- src/app/quran/page.tsx
+++ src/app/quran/page.tsx
@@ -1,56 +1,72 @@
 import React from 'react'
-import { getAllQuran } from '@/libs/api/alquran'
-import Link from 'next/link'
-import CardQuran from '@/components/Quran/CardQuran'
+import { getAllSurat } from '@/libs/api/alquran'
+import HeaderQuran from '@/components/Quran/HeaderQuran'
+import ListSurat from '@/components/Quran/ListSurat'
+import PopulerSuratCard from '@/components/Quran/PopulerSuratCard'
 export const Page = async () => {
-    const qurans = await getAllQuran()
+    const qurans = await getAllSurat()
     return (
-        <>
-            <header className="text-center py-10">
-                <h1 className="text-5xl uppercase">
-                    Listen and Read
-                    <span className="font-bold"> AL - Quran</span> Properly
-                </h1>
-            </header>
-            <main className="bg-accent px-5">
-                <section className="populer text-white py-5">
-                    <h2 className="md:text-3xl text-xl mb-5">Surat Populer</h2>
-                    <div className="grid lg:grid-cols-2 grid-cols-1 gap-5">
-                        <CardQuran />
-                        <CardQuran />
+        <div className="dark:bg-font">
+            <HeaderQuran />
+            <main className="bg-accent px-10 relative z-10 ">
+                <section className="container-populer text-white ">
+                    <h2 className="text-3xl mb-3">Surat Populer</h2>
+                    <div
+                        className="overflow-x-auto py-10 scroll-smooth"
+                        style={{
+                            scrollbarWidth: 'thin',
+                        }}
+                    >
+                        <div className="grid grid-cols-6 gap-4 w-max ">
+                            <PopulerSuratCard
+                                id={1}
+                                nama="Al-Fatihah"
+                                arti="Pembukaan"
+                                ayat={7}
+                                asma="الفاتحة"
+                            />
+                            <PopulerSuratCard
+                                id={18}
+                                nama="Al Kahfi"
+                                arti="Gua"
+                                ayat={110}
+                                asma="الكهف"
+                            />
+                            <PopulerSuratCard
+                                id={36}
+                                nama="Yaa Siin"
+                                arti="YaSin"
+                                ayat={83}
+                                asma="يس"
+                            />
+                            <PopulerSuratCard
+                                id={55}
+                                nama="Ar Rahmaan"
+                                arti="Yang Maha Pemurah"
+                                ayat={78}
+                                asma="الرحمن"
+                            />
+                            <PopulerSuratCard
+                                id={56}
+                                nama="Al Waaqi'ah"
+                                arti="Hari Kiamat"
+                                ayat={78}
+                                asma="الواقعة"
+                            />
+                            <PopulerSuratCard
+                                id={67}
+                                nama="Al Mulk"
+                                arti="Kerajaan"
+                                ayat={30}
+                                asma="الملك"
+                            />
+                        </div>
                     </div>
                 </section>
-                <section className="list text-white py-5">
-                    <h2 className="text-3xl mb-3">Daftar Surat</h2>
-                    <div className="bg-primary rounded-2xl px-2 py-3 flex flex-col gap-3 ">
-                        {qurans.map(
-                            (
-                                quran: {
-                                    nama: string
-                                    nomor: string
-                                    arti: string
-                                    ayat: number
-                                },
-                                i: number,
-                            ) => {
-                                return (
-                                    <Link
-                                        href={`/quran/${quran.nomor}`}
-                                        key={i}
-                                        className="bg-accent px-3 py-2 flex rounded-xl justify-between lg:text-lg text-xs items-center"
-                                    >
-                                        <p>{quran.nomor}</p>
-                                        <p>{quran.nama}</p>
-                                        <p>{quran.arti}</p>
-                                        <p>{quran.ayat} Ayat</p>
-                                    </Link>
-                                )
-                            },
-                        )}
-                    </div>
-                </section>
+
+                <ListSurat qurans={qurans} />
             </main>
-        </>
+        </div>
     )
 }
 
